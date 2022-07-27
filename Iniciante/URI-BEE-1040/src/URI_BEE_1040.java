cfa9109db4c1c64d72a5b586584071e32d5f712d
guinascimentoo <guilherme_nascimeento18@outlook.com>
2022-07-26 22:34:45 -0300

    atualização

--- Iniciante/URI-BEE-1040/src/URI_BEE_1040.java
+++ Iniciante/URI-BEE-1040/src/URI_BEE_1040.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,12 +6,34 @@ public class URI_BEE_1040 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.0");
 
-        double n1 = sc.nextDouble();
-        double n2 = sc.nextDouble();
-        double n3 = sc.nextDouble();
-        double n4 = sc.nextDouble();
+        double N1 = sc.nextDouble();
+        double N2 = sc.nextDouble();
+        double N3 = sc.nextDouble();
+        double N4 = sc.nextDouble();
 
+        double media = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10;
 
+        System.out.println("Media: " + df.format(media));
+
+        if(media <= 4.9 || media >= 7.0) {
+            System.out.println((media >= 7.0) ? "Aluno aprovado." : "Aluno reprovado.");
+        }
+
+        if(media >= 5.0 && media <= 6.9){
+            System.out.println("Aluno em exame.");
+            double notaExame = sc.nextDouble();
+            System.out.println("Nota do exame: " + df.format(notaExame));
+            media = (notaExame + media) / 2;
+
+            if(media >= 5.0){
+                System.out.println("Aluno aprovado.");
+            }
+            else {
+                System.out.println("Aluno reprovado.");
+            }
+            System.out.println("Media final: " + df.format(media));
+        }
     }
 }
