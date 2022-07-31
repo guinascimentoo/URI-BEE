a836471439fee2d4a6ace097e44e7ccac4bc424f
guinascimentoo <guilherme_nascimeento18@outlook.com>
2022-07-31 10:22:42 -0300

    atualização

--- Iniciante/URI-BEE-1036/src/URI_BEE_1036.java
+++ Iniciante/URI-BEE-1036/src/URI_BEE_1036.java
@@ -6,6 +6,7 @@ public class URI_BEE_1036 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00000");
 
         double A = sc.nextDouble();
         double B = sc.nextDouble();
@@ -14,8 +15,6 @@ public class URI_BEE_1036 {
         double delta = Math.sqrt(Math.pow(B, 2) - 4 * A * C);
         double x1 = (- B + delta) / (2 * A);
         double x2 = (- B - delta) / (2 * A);
-
-        DecimalFormat df = new DecimalFormat("#.#####");
 
         System.out.println((A == 0) | (B * B - 4 * A * C < 0) ? "Impossivel calcular" : "R1 = " + df.format(x1) + "\nR2 = " + df.format(x2));
     }
