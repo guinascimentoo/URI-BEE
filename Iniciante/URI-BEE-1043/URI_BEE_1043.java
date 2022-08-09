5693607ad013473d8728a8c79748b04b0c93b1c7
guinascimentoo <guilherme_nascimeento18@outlook.com>
2022-08-08 22:45:06 -0300

    atualização

---
+++ Iniciante/URI-BEE-1043/URI_BEE_1043.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class URI_BEE_1043 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+
+        if ((A + B) > C && (A + C) > B && (B + C) > A){
+            System.out.printf("Perimetro = %.1f%n", (A + B + C));
+        }
+        else{
+            System.out.printf("Area = %.1f%n", ((A + B) * C) / 2);
+        }
+    }
+}
