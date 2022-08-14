da447978c6ec6cdd765f74d6b6ca9ba18515c394
guinascimentoo <guilherme_nascimeento18@outlook.com>
2022-08-14 13:57:11 -0300

    atualização

--- Iniciante/URI-BEE-1072/URI_BEE_1072.java
+++ Iniciante/URI-BEE-1072/URI_BEE_1072.java
@@ -5,13 +5,13 @@ public class URI_BEE_1072 {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
-        int x[] = new int[num];
+        int vetor[] = new int[num];
 
         int in = 0, out = 0;
 
-        for (int i = 0; i < x.length; i++) {
-            x[i] = sc.nextInt();
-            if (x[i] >= 10 && x[i] <= 20) {
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = sc.nextInt();
+            if (vetor[i] >= 10 && vetor[i] <= 20) {
                 in++;
             } else {
                 out++;
