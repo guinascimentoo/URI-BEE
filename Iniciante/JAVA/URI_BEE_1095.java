862618656c13f30257f7c1067257c0cc1eeeb2fa
guinascimentoo <guilherme_nascimeento18@outlook.com>
2022-10-19 18:53:07 -0300

    atualização

--- Iniciante/JAVA/URI_BEE_1095.java
+++ Iniciante/JAVA/URI_BEE_1095.java
@@ -1,7 +1,7 @@
 public class URI_BEE_1095 {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i += 2) {
-            for (int I = i, j = 7; (j >= 5); j-=1) {
+            for (int I = i, j = 7; (j >= 5); j -= 1) {
                 System.out.println("I="+ I + " J=" + j);
             }
         }
--- Iniciante/JAVA/URI_BEE_1097.java
+++ Iniciante/JAVA/URI_BEE_1097.java
@@ -1,6 +1,6 @@
 public class URI_BEE_1097 {
     public static void main(String[] args) {
-        for (int i = 1,j = 7; i <= 9; i += 2,j += 2) {
+        for (int i = 1,j = 7; i <= 9; i += 2, j += 2) {
             for (int l = i, k = j; k >= (j - 2); k--) {
                 System.out.println("I=" + l + " J=" + k);
             }
